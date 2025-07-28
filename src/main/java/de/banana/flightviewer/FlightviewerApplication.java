f05cda6a8a16b63e377382a2f300b9120405aa7c
Butterbananenbrot <jh.hagedorn@gmail.com>
2025-07-28 23:45:09 +0200

    set up basic project structure. TODO: classes

---
+++ src/main/java/de/banana/flightviewer/FlightviewerApplication.java
@@ -0,0 +1,13 @@
+package de.banana.flightviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightviewerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightviewerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/banana/flightviewer/FlightviewerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.banana.flightviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightviewerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
